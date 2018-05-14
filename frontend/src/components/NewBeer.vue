60cd25fa5720957e40d9ddd596526eed63e335ab
phriscage <phriscage@gmail.com>
2018-05-13 20:28:23 -0400

    updated word-wrap for pre

--- frontend/src/components/NewBeer.vue
+++ frontend/src/components/NewBeer.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <div class="ui grid">
+   <div class="ui grid">
      <div class="row">
-       <div class="three wide column"></div>
-       <div class="ten wide column">
          <div class="ui error message" v-if="error">
            <i v-on:click="onErrorClose()" class="close icon"></i>
            <div class="header">Something broke!</div>
            <div id="error"><pre>{{ errorMessage }}</pre></div>
          </div>
-       </div>
       </div>
     </div>
+    </br>
     <div class="container">
     <vue-form id="new_beer" class="ui warning error success form" :state="formstate" @submit.prevent="onSubmit">
       <div class="ui clearing segment">
@@ -50,6 +48,23 @@
         </validate>
         <p/>
 
+        <validate auto-label class="field">
+        <label>Style:</label>
+         <div class="ui left icon input">
+           <i class="lock icon"></i>
+            <input
+               type="text"
+               name="style"
+               required
+               v-model.lazy="model.style"
+            />
+           </div>
+          <field-messages name="style" show="$touched || $submitted" :class="fieldMessageClassName(formstate.style)">
+            <div slot="required">Style is a required to identify the beer</div>
+          </field-messages>
+        </validate>
+        <p/>
+
         <button type="submit" class="ui fluid large blue submit button">Save</button>
         <div v-if="model.success" class="ui success message">
           <i v-on:click="onSuccessClose()" class="close icon"></i>
@@ -74,6 +89,7 @@ export default {
       model: {
         name: '',
         brewery: '',
+        style: '',
         success: false
       },
       context: 'new_beer context'
@@ -110,7 +126,7 @@ export default {
         return 'ui success message'
       }
       if ((field.$touched || field.$submitted) && field.$invalid) {
-        if (field.$name === 'brewery') {
+        if (field.$name === 'brewery' || field.$name === 'style') {
           return 'ui warning message'
         } else {
           return 'ui error message'
@@ -131,7 +147,8 @@ export default {
           url: _this.$shared.beersApiBaseUrl + '/beers',
           data: {
             name: _this.model.name,
-            brewery: _this.model.brewery
+            brewery: _this.model.brewery,
+            style: _this.model.style
           }
         })
         .then(function (response) {
@@ -169,6 +186,13 @@ export default {
 }
 #error {
   text-align: left;
-  width: 340px;
+  width: 600px;
+}
+pre {
+  white-space: pre-wrap; /* css-3 */
+  white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+  white-space: -pre-wrap; /* Opera 4-6 */
+  white-space: -o-pre-wrap; /* Opera 7 */
+  word-wrap: break-word; /* Internet Explorer 5.5+ */
 }
 </style>
