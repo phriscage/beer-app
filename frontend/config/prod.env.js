3039a279f15f66e498e89e0597e9151fd076e404
phriscage <phriscage@gmail.com>
2018-07-13 08:44:17 -0400

    updated for CLIENT_SECRET

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   API_BASE_URL: JSON.stringify(process.env.API_BASE_URL) || '"http://localhost:5000/api"',
   CLIENT_ID: JSON.stringify(process.env.CLIENT_ID) || '"TBD"',
+  CLIENT_SECRET: JSON.stringify(process.env.CLIENT_SECRET) || '"TBD"',
   GOOGLE_CLIENT_ID: JSON.stringify(process.env.GOOGLE_CLIENT_ID) || '"TBD"'
 }
